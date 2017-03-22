4cb5d58f476cdc6edc27e38c9fa5d86fe32f3a64
Alex Shcherbakov <alexander@prolificinteractive.com>
2017-03-22 08:22:18 -0400

    Initial Commit

---
+++ TimeSlowerDataBase/TimeSlowerDataBase.h
@@ -0,0 +1,19 @@
+//
+//  TimeSlowerDataBase.h
+//  TimeSlowerDataBase
+//
+//  Created by Alex Shcherbakov on 3/22/17.
+//  Copyright © 2017 Alex Shcherbakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TimeSlowerDataBase.
+FOUNDATION_EXPORT double TimeSlowerDataBaseVersionNumber;
+
+//! Project version string for TimeSlowerDataBase.
+FOUNDATION_EXPORT const unsigned char TimeSlowerDataBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimeSlowerDataBase/PublicHeader.h>
+
+
